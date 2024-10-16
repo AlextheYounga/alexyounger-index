fb85611dcd6f3b4e1a98a94b24eb6e3b729457ea
Alex Younger <thealexyounger@proton.me>
2024-10-16 12:34:38 -0500

    feat: add index page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,47 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="w-full">
+    <div class="flex flex-wrap justify-center">
+      <div class="w-full p-4">
+        <a href="https://alextheyounger.me" class="block">
+          <div class="flex bg-stone-800 shadow-md rounded-lg overflow-hidden">
+            <div class="w-1/3 h-44">
+              <img src="@/assets/images/alextheyounger.png.webp" alt="My Website" class="object-cover h-full w-full object-top">
+            </div>
+            <div class="w-2/3 p-4">
+              <h2 class="text-xl font-bold mb-2 text-gray-50">My Website</h2>
+              <p class="text-gray-200">This is the description for the first card.</p>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="w-full p-4">
+        <a href="https://forgecerebral.com" class="block">
+          <div class="flex bg-stone-800 shadow-md rounded-lg overflow-hidden">
+            <div class="w-1/3 h-44">
+              <img src="@/assets/images/story_of_man.jpeg.webp" alt="Forge Cerebral" class="object-cover h-full w-full">
+            </div>
+            <div class="w-2/3 p-4">
+              <h2 class="text-xl font-bold mb-2 text-gray-50">Forge Cerebral Blog</h2>
+              <p class="text-gray-200">This is the description for the first card.</p>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="w-full p-4">
+        <a href="https://earth.alexyounger.me" class="block">
+          <div class="flex bg-stone-800 shadow-md rounded-lg overflow-hidden">
+            <div class="w-1/3 h-44">
+              <img src="@/assets/images/earth.png.webp" alt="Starlink Satellite Tracker" class="object-cover h-full w-full">
+            </div>
+            <div class="w-2/3 p-4">
+              <h2 class="text-xl font-bold mb-2 text-gray-50">Starlink Satellite Tracker</h2>
+              <p class="text-gray-200">This is the description for the second card.</p>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+
+
 </template>
