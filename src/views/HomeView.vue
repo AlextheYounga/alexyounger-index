fff0d7f0be6eef78a6f2a158d251905139918a8b
Alex Younger <thealexyounger@proton.me>
2024-10-16 12:43:55 -0500

    feat: updating descriptions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,7 @@
             </div>
             <div class="w-2/3 p-4">
               <h2 class="text-xl font-bold mb-2 text-gray-50">My Website</h2>
-              <p class="text-gray-200">This is the description for the first card.</p>
+              <p class="text-gray-200">My personal website, featuring an explorable universe generator.</p>
             </div>
           </div>
         </a>
@@ -22,20 +22,20 @@
             </div>
             <div class="w-2/3 p-4">
               <h2 class="text-xl font-bold mb-2 text-gray-50">Forge Cerebral Blog</h2>
-              <p class="text-gray-200">This is the description for the first card.</p>
+              <p class="text-gray-200">Some thoughts that I hope outlast me.</p>
             </div>
           </div>
         </a>
       </div>
       <div class="w-full p-4">
-        <a href="https://earth.alexyounger.me" class="block">
+        <a href="https://starlink.alexyounger.me" class="block">
           <div class="flex bg-stone-800 shadow-md rounded-lg overflow-hidden">
             <div class="w-1/3 h-44">
               <img src="@/assets/images/earth.png.webp" alt="Starlink Satellite Tracker" class="object-cover h-full w-full">
             </div>
             <div class="w-2/3 p-4">
               <h2 class="text-xl font-bold mb-2 text-gray-50">Starlink Satellite Tracker</h2>
-              <p class="text-gray-200">This is the description for the second card.</p>
+              <p class="text-gray-200">A simulation I made to track Starlink satellites in orbit.</p>
             </div>
           </div>
         </a>
