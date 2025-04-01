9ee2207b2bad40abf769f2d414b01847e985d976
Alex Younger <thealexyounger@proton.me>
2025-04-01 11:50:02 -0500

    feat: update links and change bg color

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -28,14 +28,14 @@
         </a>
       </div>
       <div class="w-full p-4">
-        <a href="https://starlink.alexyounger.me" class="block">
+        <a href="https://resume.alexyounger.me" class="block">
           <div class="flex bg-stone-800 shadow-md rounded-lg overflow-hidden">
             <div class="w-1/3 h-44">
-              <img src="@/assets/images/earth.png.webp" alt="Starlink Satellite Tracker" class="object-cover h-full w-full">
+              <img src="@/assets/images/resume.png.webp" alt="Resume" class="object-cover h-full w-full">
             </div>
             <div class="w-2/3 p-4">
-              <h2 class="text-xl font-bold mb-2 text-gray-50">Starlink Satellite Tracker</h2>
-              <p class="text-gray-200">A simulation I made to track Starlink satellites in orbit.</p>
+              <h2 class="text-xl font-bold mb-2 text-gray-50">My Resume</h2>
+              <p class="text-gray-200">Hire me!</p>
             </div>
           </div>
         </a>
